37fb2d320109103919340f4385a8a06a7a686876
Simon Knittel <hallo@simonknittel.de>
2020-10-15 13:10:20 +0200

    Add preset switch via query parameter

--- src/assets/scripts/_modules/animated-background-1/index.ts
+++ src/assets/scripts/_modules/animated-background-1/index.ts
@@ -1,4 +1,13 @@
 import AnimationLibrary from "./animation-library";
 import Clovers from "./presets/clovers";
+import Flowers from "./presets/flowers";
 
-new AnimationLibrary(document.body, Clovers)
+switch (new window.URLSearchParams(window.location.search).get('preset')) {
+  case 'flowers':
+    new AnimationLibrary(document.body, Flowers)
+    break
+
+  default:
+    new AnimationLibrary(document.body, Clovers)
+    break
+}
